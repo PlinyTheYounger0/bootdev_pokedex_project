7279f5a4d8a1f89aff681a855fc59b3c3dc64986
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-03-01 17:49:11 -0800

    Ch. 1 L. 4 Complete

--- src/repl.ts
+++ src/repl.ts
@@ -1,10 +1,31 @@
+import { createInterface } from "node:readline";
 
 export function cleanInput(input: string): string[] {
     const inputs = input
         .toLowerCase()
         .trim()
         .split(" ")
-        .filter(word => word !== "")
+        .filter(word => word !== "");
 
-    return inputs
+    return inputs;
+}
+
+export function startREPL(): void {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    })
+    rl.prompt()
+    rl.on("line", async (input) => {
+        const inputs = cleanInput(input);
+        if (inputs.length > 0) {
+            const commandName = inputs[0]
+            console.log(`Your command was: ${commandName}`);
+            rl.prompt()
+        } else {
+            rl.prompt;
+            return;
+        }
+    });
 }
